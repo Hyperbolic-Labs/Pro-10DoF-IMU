b547a37b5db8223ab413d7e21c1537a4737027c6
Hyperbolic-Labs <61431309+Hyperbolic-Labs@users.noreply.github.com>
2020-02-24 12:25:25 -0800

    Added AHRS functionality

--- Pro_10DOF_IMU_Test.ino
+++ Pro_10DOF_IMU_Test.ino
@@ -1,37 +1,88 @@
 
-#include "Wire.h"
 
+#include "Wire.h"
+#include "MahonyAHRS.h"
+#include "MadgwickAHRS.h"
+
+#define ODR_12HZ      0b00010000
+#define ODR_26HZ      0b00100000
+#define ODR_52HZ      0b00110000
+#define ODR_104HZ     0b01000000
+#define ODR_208HZ     0b01010000
+#define ODR_416HZ     0b01100000
+#define ODR_833HZ     0b01110000
+#define ODR_1666HZ    0b10000000
+#define ODR_3333HZ    0b10010000
+#define ODR_6666HZ    0b10100000
+
+#define ACCEL_2G      0b0000
+#define ACCEL_4G      0b1000
+#define ACCEL_8G      0b1100
+#define ACCEL_16G     0b0100
+
+#define GYRO_125DPS   0b0010
+#define GYRO_250DPS   0b0000
+#define GYRO_500DPS   0b0100
+#define GYRO_1000DPS  0b1000
+#define GYRO_2000DPS  0b1100
 
 #define DEV_ADDR      0x6B
-#define LSB_PER_G     (float)(32768.0 / 2.0)
-#define LSB_PER_DPS   (float)(32768.0 / 500.0)
+#define ACCEL_RANGE   ACCEL_4G
+#define GYRO_RANGE    GYRO_1000DPS
+#define UPDATE_RATE   400
+
+#define INVERT_X_AXIS   true
+#define INVERT_Y_AXIS   true
+#define INVERT_Z_AXIS   false
+//#define VERBOSE
+
+#define LSB_PER_G     (float)(32768.0 / 4.0)
+#define LSB_PER_DPS   (float)(32768.0 / 1000.0)
 #define LSB_PER_C     (float)(256.0)
 
+
 float axf, ayf, azf, gxf, gyf, gzf, tempf;
+float yaw, pitch, roll;
 int16_t ax, ay, az, gx, gy, gz, temp;
+
+
+//Mahony filter;
+Madgwick filter;
 
 
 void setup(){
 
-  Serial.begin(115200);
+  Serial.begin(230400);
   Wire.begin();
-
+  filter.begin(UPDATE_RATE);
 
   Serial.print("Device ID: ");
   Serial.println(readReg(0x0F), HEX);
 
-  writeReg(0x10, 0x60); //CTRL1_XL, 416HZ, +-2G
-  writeReg(0x11, 0x60); //CTRL1_XL, 416HZ, +-250dps  
+  writeReg(0x10, ODR_416HZ | ACCEL_RANGE); 
+  writeReg(0x11, ODR_416HZ | GYRO_RANGE); 
 }
 
 
 
 void loop(){
 
+  uint32_t us = micros();
+
   readGyroRaw(gx, gy, gz);
   readAccelRaw(ax, ay, az);
-  readTempRaw(temp);
-  
+  readTempRaw(temp); 
+
+  getFloatAccel(ax, ay, az, axf, ayf, azf);
+  getFloatGyro( gx, gy, gz, gxf, gyf, gzf);
+  getFloatTemp(temp, tempf);
+
+  filter.updateIMU(gxf, gyf, gzf, axf, ayf, azf);
+  roll  = filter.getRoll();
+  pitch = filter.getPitch();
+  yaw   = filter.getYaw();
+
+#ifdef VERBOSE
   Serial.print("ax: "); Serial.print(ax); Serial.print("\t");
   Serial.print("ay: "); Serial.print(ay); Serial.print("\t");
   Serial.print("az: "); Serial.print(az); Serial.print("\t\t");
@@ -42,11 +93,7 @@ void loop(){
 
   Serial.print("temp: "); Serial.print(temp); Serial.print("\t");
   Serial.println();
-
-  getFloatAccel(ax, ay, az, axf, ayf, azf);
-  getFloatGyro( gx, gy, gz, gxf, gyf, gzf);
-  getFloatTemp(temp, tempf);
-
+  
   Serial.print("axf: "); Serial.print(axf); Serial.print("\t");
   Serial.print("ayf: "); Serial.print(ayf); Serial.print("\t");
   Serial.print("azf: "); Serial.print(azf); Serial.print("\t");
@@ -57,24 +104,56 @@ void loop(){
 
   Serial.print("temp: "); Serial.print(tempf); Serial.print("\t");
   Serial.println();
-
-  delay(1000);
   
+  Serial.print("Yaw: ");   Serial.print(yaw);   Serial.print("\t");
+  Serial.print("Pitch: "); Serial.print(pitch); Serial.print("\t");
+  Serial.print("Roll: ");  Serial.print(roll);  Serial.print("\t");
+#else
+
+  Serial.print("Orientation: ");
+  Serial.print(yaw);
+  Serial.print(" ");
+  Serial.print(pitch);
+  Serial.print(" ");
+  Serial.println(roll);
+#endif
+
+  
+
+  delayMicroseconds((1000000/UPDATE_RATE)-(micros()-us));  
 }
 
 
 void getFloatAccel(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float &zf){
 
-  xf = (float)(x)/LSB_PER_G;
-  yf = (float)(y)/LSB_PER_G;
-  zf = (float)(z)/LSB_PER_G;
+  if(INVERT_X_AXIS)
+    xf = -(float)(x)/LSB_PER_G;
+  else
+    xf = (float)(x)/LSB_PER_G;
+  if(INVERT_Y_AXIS)
+    yf = -(float)(y)/LSB_PER_G;
+  else
+    yf = (float)(y)/LSB_PER_G;
+  if(INVERT_Z_AXIS)
+    zf = -(float)(z)/LSB_PER_G;
+  else
+    zf = (float)(z)/LSB_PER_G;
 }
 
 void getFloatGyro(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float &zf){
 
-  xf = (float)(x)/LSB_PER_DPS;
-  yf = (float)(y)/LSB_PER_DPS;
-  zf = (float)(z)/LSB_PER_DPS;
+  if(INVERT_X_AXIS)
+    xf = -(float)(x)/LSB_PER_DPS;
+  else
+    xf = (float)(x)/LSB_PER_DPS;
+  if(INVERT_Y_AXIS)
+    yf = -(float)(y)/LSB_PER_DPS;
+  else
+    yf = (float)(y)/LSB_PER_DPS;
+  if(INVERT_Z_AXIS)
+    zf = -(float)(z)/LSB_PER_DPS;
+  else
+    zf = (float)(z)/LSB_PER_DPS;
 }
 
 void getFloatTemp(int16_t t, float &tf){
@@ -129,3 +208,4 @@ void writeReg(uint8_t reg, uint8_t val){
   Wire.write(val);
   Wire.endTransmission();
 }
+
