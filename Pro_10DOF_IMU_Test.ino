22d4faa2a52a594858fcda02383d942ca14fbf09
Hyperbolic-Labs <61431309+Hyperbolic-Labs@users.noreply.github.com>
2020-02-24 13:32:07 -0800

    Added Magnetometer support and 9DoF sensor fusion

--- Pro_10DOF_IMU_Test.ino
+++ Pro_10DOF_IMU_Test.ino
@@ -1,19 +1,27 @@
 
-
 #include "Wire.h"
 #include "MahonyAHRS.h"
 #include "MadgwickAHRS.h"
 
-#define ODR_12HZ      0b00010000
-#define ODR_26HZ      0b00100000
-#define ODR_52HZ      0b00110000
-#define ODR_104HZ     0b01000000
-#define ODR_208HZ     0b01010000
-#define ODR_416HZ     0b01100000
-#define ODR_833HZ     0b01110000
-#define ODR_1666HZ    0b10000000
-#define ODR_3333HZ    0b10010000
-#define ODR_6666HZ    0b10100000
+#define IMU_ODR_12HZ      0b00010000
+#define IMU_ODR_26HZ      0b00100000
+#define IMU_ODR_52HZ      0b00110000
+#define IMU_ODR_104HZ     0b01000000
+#define IMU_ODR_208HZ     0b01010000
+#define IMU_ODR_416HZ     0b01100000
+#define IMU_ODR_833HZ     0b01110000
+#define IMU_ODR_1666HZ    0b10000000
+#define IMU_ODR_3333HZ    0b10010000
+#define IMU_ODR_6666HZ    0b10100000
+
+#define MAG_ODR_1HZ       0b1001
+#define MAG_ODR_10HZ      0b1010
+#define MAG_ODR_20HZ      0b1011
+#define MAG_ODR_50HZ      0b1100
+#define MAG_ODR_100HZ     0b1101
+#define MAG_ODR_200HZ     0b1110
+#define MAG_ODR_1000HZ    0b1111
+
 
 #define ACCEL_2G      0b0000
 #define ACCEL_4G      0b1000
@@ -26,24 +34,35 @@
 #define GYRO_1000DPS  0b1000
 #define GYRO_2000DPS  0b1100
 
-#define DEV_ADDR      0x6B
+#define IMU_ADDR      0x6B
+#define MAG_ADDR      0x30
+
 #define ACCEL_RANGE   ACCEL_4G
 #define GYRO_RANGE    GYRO_1000DPS
-#define UPDATE_RATE   400
+#define IMU_RATE      IMU_ODR_416HZ
+#define MAG_RATE      MAG_ODR_200HZ
+#define MAG_HIGH_RES_MODE
 
-#define INVERT_X_AXIS   true
-#define INVERT_Y_AXIS   true
-#define INVERT_Z_AXIS   false
+
+#define FILTER_RATE   100
+
+#define IMU_INVERT_X_AXIS   true
+#define IMU_INVERT_Y_AXIS   true
+#define IMU_INVERT_Z_AXIS   false
+#define MAG_INVERT_X_AXIS   false
+#define MAG_INVERT_Y_AXIS   false
+#define MAG_INVERT_Z_AXIS   true
 //#define VERBOSE
 
-#define LSB_PER_G     (float)(32768.0 / 4.0)
-#define LSB_PER_DPS   (float)(32768.0 / 1000.0)
-#define LSB_PER_C     (float)(256.0)
+#define LSB_PER_G       (float)(32768.0 / 4.0)
+#define LSB_PER_DPS     (float)(32768.0 / 1000.0)
+#define LSB_PER_C       256
+#define LSB_PER_GAUSS   4096
 
 
-float axf, ayf, azf, gxf, gyf, gzf, tempf;
+float axf, ayf, azf, gxf, gyf, gzf, mxf, myf, mzf, tempf;
 float yaw, pitch, roll;
-int16_t ax, ay, az, gx, gy, gz, temp;
+int16_t ax, ay, az, gx, gy, gz, mx, my, mz, temp;
 
 
 //Mahony filter;
@@ -52,15 +71,16 @@ Madgwick filter;
 
 void setup(){
 
+  pinMode(13, OUTPUT);
+
   Serial.begin(230400);
   Wire.begin();
-  filter.begin(UPDATE_RATE);
+  filter.begin(FILTER_RATE);
 
-  Serial.print("Device ID: ");
-  Serial.println(readReg(0x0F), HEX);
+  writeReg(IMU_ADDR, 0x10, IMU_ODR_416HZ | ACCEL_RANGE); 
+  writeReg(IMU_ADDR, 0x11, IMU_ODR_416HZ | GYRO_RANGE); 
 
-  writeReg(0x10, ODR_416HZ | ACCEL_RANGE); 
-  writeReg(0x11, ODR_416HZ | GYRO_RANGE); 
+  writeReg(MAG_ADDR, 0x0B, MAG_ODR_200HZ);
 }
 
 
@@ -68,16 +88,20 @@ void setup(){
 void loop(){
 
   uint32_t us = micros();
+  digitalWrite(13, LOW);
 
-  readGyroRaw(gx, gy, gz);
+  readGyroRaw( gx, gy, gz);
   readAccelRaw(ax, ay, az);
-  readTempRaw(temp); 
+  readMagRaw(  mx, my, mz);
+  readIMUTempRaw(temp); 
 
   getFloatAccel(ax, ay, az, axf, ayf, azf);
   getFloatGyro( gx, gy, gz, gxf, gyf, gzf);
-  getFloatTemp(temp, tempf);
+  getFloatMag(  mx, my, mz, mxf, myf, mzf);
+  getIMUFloatTemp(temp, tempf);
 
-  filter.updateIMU(gxf, gyf, gzf, axf, ayf, azf);
+  filter.update(gxf, gyf, gzf, axf, ayf, azf, mxf, myf, mzf);
+  //filter.updateIMU(gxf, gyf, gzf, axf, ayf, azf);
   roll  = filter.getRoll();
   pitch = filter.getPitch();
   yaw   = filter.getYaw();
@@ -91,6 +115,10 @@ void loop(){
   Serial.print("gy: "); Serial.print(gy); Serial.print("\t");
   Serial.print("gz: "); Serial.print(gz); Serial.print("\t");
 
+  Serial.print("mx: "); Serial.print(mx); Serial.print("\t");
+  Serial.print("my: "); Serial.print(my); Serial.print("\t");
+  Serial.print("mz: "); Serial.print(mz); Serial.print("\t");
+
   Serial.print("temp: "); Serial.print(temp); Serial.print("\t");
   Serial.println();
   
@@ -101,6 +129,10 @@ void loop(){
   Serial.print("gxf: "); Serial.print(gxf); Serial.print("\t");
   Serial.print("gyf: "); Serial.print(gyf); Serial.print("\t");
   Serial.print("gzf: "); Serial.print(gzf); Serial.print("\t");
+
+  Serial.print("mxf: "); Serial.print(mxf); Serial.print("\t");
+  Serial.print("myf: "); Serial.print(myf); Serial.print("\t");
+  Serial.print("mzf: "); Serial.print(mzf); Serial.print("\t");
 
   Serial.print("temp: "); Serial.print(tempf); Serial.print("\t");
   Serial.println();
@@ -119,22 +151,22 @@ void loop(){
 #endif
 
   
-
-  delayMicroseconds((1000000/UPDATE_RATE)-(micros()-us));  
+  digitalWrite(13, HIGH);
+  delayMicroseconds((1000000/IMU_RATE)-(micros()-us));  
 }
 
 
 void getFloatAccel(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float &zf){
 
-  if(INVERT_X_AXIS)
+  if(IMU_INVERT_X_AXIS)
     xf = -(float)(x)/LSB_PER_G;
   else
     xf = (float)(x)/LSB_PER_G;
-  if(INVERT_Y_AXIS)
+  if(IMU_INVERT_Y_AXIS)
     yf = -(float)(y)/LSB_PER_G;
   else
     yf = (float)(y)/LSB_PER_G;
-  if(INVERT_Z_AXIS)
+  if(IMU_INVERT_Z_AXIS)
     zf = -(float)(z)/LSB_PER_G;
   else
     zf = (float)(z)/LSB_PER_G;
@@ -142,31 +174,48 @@ void getFloatAccel(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float 
 
 void getFloatGyro(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float &zf){
 
-  if(INVERT_X_AXIS)
+  if(IMU_INVERT_X_AXIS)
     xf = -(float)(x)/LSB_PER_DPS;
   else
     xf = (float)(x)/LSB_PER_DPS;
-  if(INVERT_Y_AXIS)
+  if(IMU_INVERT_Y_AXIS)
     yf = -(float)(y)/LSB_PER_DPS;
   else
     yf = (float)(y)/LSB_PER_DPS;
-  if(INVERT_Z_AXIS)
+  if(IMU_INVERT_Z_AXIS)
     zf = -(float)(z)/LSB_PER_DPS;
   else
     zf = (float)(z)/LSB_PER_DPS;
 }
 
-void getFloatTemp(int16_t t, float &tf){
+
+void getFloatMag(int16_t x, int16_t y, int16_t z, float &xf, float &yf, float &zf){
+
+  if(MAG_INVERT_X_AXIS)
+    xf = -(float)(x)/LSB_PER_GAUSS;
+  else
+    xf = (float)(x)/LSB_PER_GAUSS;
+  if(MAG_INVERT_Y_AXIS)
+    yf = -(float)(y)/LSB_PER_GAUSS;
+  else
+    yf = (float)(y)/LSB_PER_GAUSS;
+  if(MAG_INVERT_Z_AXIS)
+    zf = -(float)(z)/LSB_PER_GAUSS;
+  else
+    zf = (float)(z)/LSB_PER_GAUSS;  
+}
+
+void getIMUFloatTemp(int16_t t, float &tf){
 
   tf = 25.0 + (float)(t)/LSB_PER_C;
 }
 
 void readAccelRaw(int16_t &x, int16_t &y, int16_t &z){
 
-  Wire.beginTransmission(DEV_ADDR);
+  Wire.beginTransmission(IMU_ADDR);
   Wire.write(0x28);
   Wire.endTransmission();
-  Wire.requestFrom(DEV_ADDR, 6);
+  Wire.requestFrom(IMU_ADDR, 6);
   x = Wire.read() | (Wire.read() << 8);
   y = Wire.read() | (Wire.read() << 8);
   z = Wire.read() | (Wire.read() << 8);
@@ -174,38 +223,51 @@ void readAccelRaw(int16_t &x, int16_t &y, int16_t &z){
 
 void readGyroRaw(int16_t &x, int16_t &y, int16_t &z){
 
-  Wire.beginTransmission(DEV_ADDR);
+  Wire.beginTransmission(IMU_ADDR);
   Wire.write(0x22);
   Wire.endTransmission();
-  Wire.requestFrom(DEV_ADDR, 6);
+  Wire.requestFrom(IMU_ADDR, 6);
   x = Wire.read() | (Wire.read() << 8);
   y = Wire.read() | (Wire.read() << 8);
   z = Wire.read() | (Wire.read() << 8);
 }
 
-void readTempRaw(int16_t &t){
+void readMagRaw(int16_t &x, int16_t &y, int16_t &z){
 
-  Wire.beginTransmission(DEV_ADDR);
+  Wire.beginTransmission(MAG_ADDR);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  Wire.requestFrom(MAG_ADDR, 6);
+//  x = (Wire.read() << 8) | Wire.read();
+//  y = (Wire.read() << 8) | Wire.read();
+//  z = (Wire.read() << 8) | Wire.read();
+  x = Wire.read() | (Wire.read() << 8);
+  y = Wire.read() | (Wire.read() << 8);
+  z = Wire.read() | (Wire.read() << 8);
+}
+
+void readIMUTempRaw(int16_t &t){
+
+  Wire.beginTransmission(IMU_ADDR);
   Wire.write(0x20);
   Wire.endTransmission();
-  Wire.requestFrom(DEV_ADDR, 2);
+  Wire.requestFrom(IMU_ADDR, 2);
   t = Wire.read() | (Wire.read() << 8);
 }
 
-uint8_t readReg(uint8_t reg){
+uint8_t readReg(uint8_t addr, uint8_t reg){
 
-  Wire.beginTransmission(DEV_ADDR);
+  Wire.beginTransmission(addr);
   Wire.write(reg);
   Wire.endTransmission();
-  Wire.requestFrom(DEV_ADDR, 1);
+  Wire.requestFrom(addr, 1);
   return Wire.read();
 }
 
-void writeReg(uint8_t reg, uint8_t val){
+void writeReg(uint8_t addr, uint8_t reg, uint8_t val){
 
-  Wire.beginTransmission(DEV_ADDR);
+  Wire.beginTransmission(addr);
   Wire.write(reg);
   Wire.write(val);
   Wire.endTransmission();
 }
-
